659724ea158b4e110fcf04454d88bdd9b8517e5b
Henrique de Lima Pessoa <henriquedelimapessoa@gmail.com>
2023-04-17 12:06:55 -0300

    animations

--- src/Page/Main/MenuLateral/MenuLateral.styled.ts
+++ src/Page/Main/MenuLateral/MenuLateral.styled.ts
@@ -2,5 +2,4 @@ import { Stack } from "../../../Components";
 import styled from "@emotion/styled";
 import { IActive } from "../../../Components/Menu/MenuItem/MenuItem.type";
 export const MenuLateralStyled = styled(Stack) <IActive>`
-    transition: all 200ms ease-in-out;
 `
--- src/Page/Main/MenuLateral/MenuLateral.tsx
+++ src/Page/Main/MenuLateral/MenuLateral.tsx
@@ -8,7 +8,9 @@ export const MenuLateral = ({ children, isActive, width }: IMenuLateral) => {
     return (
         <MenuLateralStyled
             direction="column"
-            style={{ opacity: isActive ? 1 : 0 }}
+            style={{
+                opacity: isActive ? 1 : 0,
+            }}
             isActive={isActive}
             height="100%"
             width={isActive ? `${width}px` : '0px'}
