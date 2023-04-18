1e77a2c9eb298d6859116df66fb5fb5cc992430f
Henrique de Lima Pessoa <henriquedelimapessoa@gmail.com>
2023-04-17 21:50:30 -0300

    Task Type

--- src/Components/Board/Notes/Task/Task.type.ts
+++ src/Components/Board/Notes/Task/Task.type.ts
@@ -2,12 +2,14 @@ import { ITask } from "../../../../Common/Context/Notes/Notes.types";
 
 export interface ITaskContainer {
     task: ITask
+}
+export interface ITaskElement extends ITaskContainer {
     hover: boolean
     currentTask: ITask
     handleHover: () => void
 }
 
-export interface ITaskComponent extends ITaskContainer {
+export interface ITaskComponent extends ITaskElement {
     handleCurrentTask: (e: ITask) => void
     borderRadius: string
 }
